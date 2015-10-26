3f28faaab9c02bd13496ba0a08ca8615ed7a9de0
Praveen <praveen.kumar@universumglobal.com>
2015-10-26 19:38:08 +0530

    login page implemented

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+	validates :name,  presence: true, length: { maximum: 50 }
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+end
